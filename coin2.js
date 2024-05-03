cb00cdc0611de08a7b145f012ea6ca1a0e4b3558
jdkriner <jackkriner@icloud.com>
2024-05-02 23:24:56 -0700

    styling on coin2

--- coin2.js
+++ coin2.js
@@ -7,12 +7,34 @@ probabilityInput.addEventListener("input", function () {
   probabilityDisplay.textContent = this.value + "%";
 });
 
+document.addEventListener("keydown", (letter) =>{
+  console.log(letter.key);
+  if (letter.key == "Enter"){
+    flipCoin();
+  }
+
+  if (letter.key == "0"){
+    window.location.assign("homepage.html");
+  }
+
+});
+
+
+
 function flipCoin() {
   let probability = probabilityInput.value / 100;
   let result = Math.random() < probability ? "Heads" : "Tails";
   if (result == "Heads") {
-    coinFlipResult.textContent = "HEADS";
+    text = document.querySelector(".resulttext")
+    text.innerHTML = "<h1>Heads!</h1>";
+    image = document.querySelector(".coinpic");
+    image.style.backgroundImage = "url('images/heads.png')";
+
   } else {
-    coinFlipResult.textContent = "TAILS";
+    text = document.querySelector(".resulttext")
+    text.innerHTML = "<h1>Tails!</h1>";
+    image = document.querySelector(".coinpic");
+    image.style.backgroundImage = "url('images/tails.png')";
+    
   }
 }
